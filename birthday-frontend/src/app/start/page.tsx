3e7de0682f6ac84ab679ef14026b86628c1e2dbf
lyeonn <kiteyeonn@gmail.com>
2026-05-25 15:13:37 +0900

    fix: /start 페이지에서 force-dynamic으로 prerender 에러 수정

--- birthday-frontend/src/app/start/page.tsx
+++ birthday-frontend/src/app/start/page.tsx
@@ -3,6 +3,8 @@
 import { useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:3001';
 
 type Mode = 'register' | 'login';
